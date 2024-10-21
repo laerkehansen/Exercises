383e3430f7e77b5da1e33757f0dcb3aa3abc43c8
laerkehansen <laha0007@stud.kea.dk>
2024-10-21 12:15:21 +0200

    idk

---
+++ localstorage/index.js
@@ -0,0 +1,19 @@
+document.querySelector(`[data-action="set"]`).addEventListener(("click", setAndGet));
+document.querySelector(`[data-action="get"]`).addEventListener(("click", setAndGet));
+const setText = document.querySelector(".set");
+const getText = document.querySelector(".get");
+
+function setAndGet(){
+const target = evt.target;
+const action = target.dataset. action;
+
+console.log("target", target);
+console.log("action", action);
+
+if (action === "set"){
+    localStorage.setItem("myDog", setText.value);
+}
+if (action === "get"){
+    getText.value = localStorage.getItem("myDog");
+}
+}
